a52bafe9618f55cfff52274d0b82d42c8369fc66
Jeewoo Lee <jlee@lcs.on.ca>
2020-02-16 13:11:21 -0500

    made it work

--- War/main.swift
+++ War/main.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //create a Suit enumeration
 enum Suit: Int {
-    case heart = 0, spade, diamond, club
+    case heart = 1, spade, diamond, club
 }
 
 //create a Value enumeration
@@ -51,28 +51,29 @@ struct beggarYourNeighbour {
     // Empty deck of cards
     var deck: [Card] = []
     
-        
+    var countTurns = 1
     var isPlayerTurn = true
     var player: [Card] = []
     var computer: [Card] = []
     var middle: [Card] = []
+    var isGameOver = true
     
-    
-    mutating func generateDeck(deck: [Card]) -> [Card] {
-        var fullDeck = deck
+    mutating func generateDeck() {
+        
         
         // Populating the deck
         for i in 1...4 {
             for j in 2...14 {
-                fullDeck.append(Card(suit: Suit(rawValue: i)!, value: Value(rawValue: j)!))
+                deck.append(Card(suit: Suit(rawValue: i)!, value: Value(rawValue: j)!))
             }
         }
         
-        return fullDeck
+        
     }
     
     // Deal cards to both of the players' hands
     mutating func dealCards() {
+        generateDeck()
         while deck.count > 0 {
             
             // Get a random value between 0 and the end of the deck array
@@ -93,36 +94,65 @@ struct beggarYourNeighbour {
             // Remove the card from the deck
             deck.remove(at: randomCardPosition)
             
-            print(randomCardPosition)
+            //print(randomCardPosition)
         }
         
         
     }
     
     mutating func addToMiddle(theCard: Card, playerTurn: Bool) {
-        middle.append(theCard)
-        if playerTurn{
-            player.remove(at: 0)
-        } else{
-            computer.remove(at: 0)
+        if player.count < 1 {
+            whoIsWinner(winner: "Computer")
+        } else if computer.count < 1 {
+            whoIsWinner(winner: "Player")
+        }else {
+            print("Computer has \(computer.count) cards")
+            print("Player has \(player.count) cards")
+            middle.insert(theCard, at: 0)
+            if playerTurn{
+                
+                    player.remove(at: 0)
+                    print("Player put \(theCard.suit) \(theCard.value)")
+                    print("========================================================")
+                
+                
+            } else{
+                
+                    computer.remove(at: 0)
+                    print("Computer put \(theCard.suit) \(theCard.value)")
+                    print("========================================================")
+                
+                
+            }
         }
+        
 
     }
 
-    mutating func check(theCard: Card, playerTurn: Bool) {
-        if theCard.value.rawValue >= 11 {
-            //repeat
-            if playerTurn {
-                isPlayerTurn.toggle()
-                repeatComputer(thePlayerCard: theCard)
-            } else {
-                isPlayerTurn.toggle()
-                repeatPlayer(theComputerCard: theCard)
-            }
-        } else{
-            isPlayerTurn.toggle()
-            playTheGame()
+    mutating func check(theCard: Card, playerTurn: Bool, isRepeating: Bool) {
+        if computer.count < 1{
+            whoIsWinner(winner: "Player")
+            
         }
+        else if player.count < 1{
+            whoIsWinner(winner: "Computer")
+            
+        } else {
+            if theCard.value.rawValue >= 11 {
+                //repeat
+                if playerTurn {
+                    isPlayerTurn.toggle()
+                    repeatComputer(thePlayerCard: theCard)
+                } else {
+                    isPlayerTurn.toggle()
+                    repeatPlayer(theComputerCard: theCard)
+                }
+            } else if isRepeating == false{
+                  isPlayerTurn.toggle()
+                  playTheGame()
+            }
+        }
+        
         
     }
     
@@ -130,46 +160,100 @@ struct beggarYourNeighbour {
     mutating func repeatComputer (thePlayerCard: Card) {
         var repeatedTimes = 1
         for _ in 0...thePlayerCard.value.rawValue - 10 {
-            addToMiddle(theCard: computer[0], playerTurn: isPlayerTurn)
-            check(theCard: middle[0], playerTurn: isPlayerTurn)
-            repeatedTimes += 1
+            if computer.count > 0 {
+                addToMiddle(theCard: computer[0], playerTurn: isPlayerTurn)
+                if middle.count > 0 {
+                    check(theCard: middle[0], playerTurn: isPlayerTurn, isRepeating: true)
+                }
+                repeatedTimes += 1
+                
+            }
         }
         if repeatedTimes == thePlayerCard.value.rawValue - 10 {
             player.append(contentsOf: middle)
             middle.removeAll()
+            isPlayerTurn.toggle()
         }
     }
     
     mutating func repeatPlayer (theComputerCard: Card) {
         var repeatedTimes = 1
         for _ in 0...theComputerCard.value.rawValue - 10 {
-            //check()
-            addToMiddle(theCard: player[0], playerTurn: isPlayerTurn)
-            check(theCard: middle[0], playerTurn: isPlayerTurn)
-            repeatedTimes += 1
+            if player.count > 0 {
+                addToMiddle(theCard: player[0], playerTurn: isPlayerTurn)
+                if middle.count > 0 {
+                    check(theCard: middle[0], playerTurn: isPlayerTurn, isRepeating: true)
+                }
+                repeatedTimes += 1
+            }
+            
         }
         if repeatedTimes == theComputerCard.value.rawValue - 10 {
             computer.append(contentsOf: middle)
+            middle.removeAll()
+            isPlayerTurn.toggle()
         }
     }
     
     mutating func playTheGame() {
-        if isPlayerTurn {
-            addToMiddle(theCard: player[0], playerTurn: isPlayerTurn)
-            check(theCard: middle[0], playerTurn: isPlayerTurn)
-        } else {
-            addToMiddle(theCard: computer[0], playerTurn: isPlayerTurn)
-            check(theCard: middle[0], playerTurn: isPlayerTurn)
+        
+        if computer.count < 1{
+            whoIsWinner(winner: "Player")
+            
+        }
+        else if player.count < 1{
+            whoIsWinner(winner: "Computer")
+            
+        }
+        else {
+            if isPlayerTurn {
+                addToMiddle(theCard: player[0], playerTurn: isPlayerTurn)
+                check(theCard: middle[0], playerTurn: isPlayerTurn, isRepeating: false)
+                
+            } else {
+                addToMiddle(theCard: computer[0], playerTurn: isPlayerTurn)
+                check(theCard: middle[0], playerTurn: isPlayerTurn, isRepeating: false)
+                
+            }
         }
         
     }
     
     mutating func start() {
+        while isGameOver == true {
+            dealCards()
+            isGameOver = false
         
+        }
+        
+        while isGameOver == false {
+            playTheGame()
+    }
+  }
+    
+    mutating func whoIsWinner(winner: String) {
+        
+        if winner == "Player" && isGameOver == false
+        {
+            print("========================================================")
+            print("VICTORY")
+            isGameOver = true
+            
+        }
+        else if winner == "Computer" && isGameOver == false
+        {
+            print("========================================================")
+            print("DEFEATED")
+            isGameOver = true
+
+        }
     }
     
     
     
 }
+
+var newGame = beggarYourNeighbour()
+newGame.start()
 
 
